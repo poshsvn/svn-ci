377049fd2d93c32967bbaa76c0f8787bcd1aedc2
Timofei Zhakov <tima@chemodax.net>
2024-06-21 21:48:04 +0200

    Factor-out Get-SvnSource script

---
+++ scripts/Get-SvnSource.ps1
@@ -0,0 +1,14 @@
+param (
+    $Url,
+    $Revision
+)
+
+if ($url -eq "") {
+    $url = "https://svn.apache.org/repos/asf/subversion/trunk"
+}
+
+if ($revision -eq "") {
+    $revision = "HEAD"
+}
+
+svn checkout $url --revision $revision svn
