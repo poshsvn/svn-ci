7e7578ef08d7d3bc448919e30816249d62c16d54
Timofei Zhakov <tima@chemodax.net>
2024-06-23 00:09:59 +0200

    add build for x86

--- scripts/Setup-VsDevShell.ps1
+++ scripts/Setup-VsDevShell.ps1
@@ -1,3 +1,7 @@
+param(
+    $arch
+)
+
 $root = "C:\Program Files\Microsoft Visual Studio\2022\Enterprise"
 Import-Module "$root\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
-Enter-VsDevShell -VsInstallPath $root -DevCmdArguments "-arch=x64 -host_arch=x64"
+Enter-VsDevShell -VsInstallPath $root -DevCmdArguments "-arch=$arch -host_arch=x64"
